68a66a43db5cb4c5493eb4441fa794ba038e1a1d
S.muneiwa <128157791+moneiwa@users.noreply.github.com>
2024-11-19 16:19:57 +0200

    Update server.js

--- nodejs-server-app/server.js
+++ nodejs-server-app/server.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const cors = require('cors');
-const { db } = require('./Controllers/db');  
-const { collection, getDocs, doc, addDoc, deleteDoc, updateDoc } = require('firebase/firestore');
+const { db } = require('./Controllers/db');
+const { collection, getDocs, addDoc, deleteDoc, updateDoc, doc } = require('firebase/firestore');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-
 
 app.use(express.json());
 app.use(cors());
@@ -27,15 +26,25 @@ app.get('/api/employees', async (req, res) => {
 
 
 app.post('/api/employees', async (req, res) => {
-  const { name, surname, email, phoneNumber, employeePosition, image } = req.body;
+  const { employeeId, name, surname, email, phoneNumber, employeePosition, image } = req.body;
 
-  
-  if (!name || !surname || !email || !phoneNumber || !employeePosition) {
+
+  if (!employeeId || !name || !surname || !email || !phoneNumber || !employeePosition) {
     return res.status(400).json({ message: "Missing required fields" });
   }
 
   try {
+   
+    const querySnapshot = await getDocs(collection(db, 'employees'));
+    const existingEmployee = querySnapshot.docs.find(doc => doc.data().employeeId === employeeId);
+
+    if (existingEmployee) {
+      return res.status(400).json({ message: "Employee ID already exists" });
+    }
+
+    
     const docRef = await addDoc(collection(db, 'employees'), {
+      employeeId,  
       name,
       surname,
       email,
@@ -43,6 +52,7 @@ app.post('/api/employees', async (req, res) => {
       employeePosition,
       image,
     });
+
     res.json({ message: "Employee added successfully", id: docRef.id });
   } catch (error) {
     console.error('Error adding employee:', error);
@@ -62,37 +72,32 @@ app.delete('/api/employees/:id', async (req, res) => {
   }
 });
 
+
 app.put('/api/employees/:id', async (req, res) => {
   const employeeId = req.params.id;
   const { name, surname, email, phoneNumber, employeePosition, image } = req.body;
-
-  console.log('Received update request for employee ID:', employeeId);
-  console.log('Data to update:', req.body);
 
   if (!name || !surname || !email || !phoneNumber || !employeePosition) {
     return res.status(400).json({ message: "Missing required fields" });
   }
 
   try {
-   
+    
     await updateDoc(doc(db, 'employees', employeeId), {
       name,
       surname,
       email,
       phoneNumber,
       employeePosition,
-      image, 
+      image,
     });
 
-    
     res.json({ message: "Employee updated successfully" });
   } catch (error) {
     console.error('Error updating employee:', error);
     res.status(500).json({ message: "Error updating employee", error: error.message });
   }
 });
-
-
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
